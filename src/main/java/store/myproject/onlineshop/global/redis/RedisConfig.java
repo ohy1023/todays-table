370da8d092b40cecd876808586f560c683776a69
ohy1023 <zvyg1023@naver.com>
2023-05-20 19:38:10 +0900

    FEAT : JWT,REDIS,SWAGGER,SECURITY

---
+++ src/main/java/store/myproject/onlineshop/global/redis/RedisConfig.java
@@ -0,0 +1,36 @@
+package store.myproject.onlineshop.global.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${REDIS_PORT}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    /**
+     * RedisTemplate: java Object를 redis에 저장하는 경우 사용
+     */
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/store/myproject/onlineshop/global/redis/RedisDao.java
@@ -0,0 +1,47 @@
+package store.myproject.onlineshop.global.redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisDao {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public RedisDao(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Transactional
+    public void setValues(String key, String value) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, value);
+    }
+
+    @Transactional
+    public void setValues(String key, String value, Duration duration) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, value, duration);
+    }
+
+    @Transactional
+    public void setValues(String key, String value, long timeout, TimeUnit unit) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, value, timeout, unit);
+    }
+
+    @Transactional
+    public String getValues(String key) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+
+    @Transactional
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+}
