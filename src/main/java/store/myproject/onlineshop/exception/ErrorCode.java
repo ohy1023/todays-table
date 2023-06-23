fb7482f86697b60fee5a1bd0ab451d7a81bc96c9
ohy1023 <zvyg1023@naver.com>
2023-06-23 21:18:07 +0900

    style : 오류 메세지 변경

--- src/main/java/store/myproject/onlineshop/exception/ErrorCode.java
+++ src/main/java/store/myproject/onlineshop/exception/ErrorCode.java
@@ -9,28 +9,27 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // Common (Authentication)
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Database Error"),
-    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Unauthorized access"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password"),
-    INVALID_REQUEST(HttpStatus.UNAUTHORIZED, "invalid quest"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token invalid"),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Token expired"),
-    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Token not found"),
-    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "Access forbidden"),
-    CONFLICT(HttpStatus.CONFLICT, "Request is Conflict"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "틀린 비밀번호입니다."),
+    INVALID_REQUEST(HttpStatus.UNAUTHORIZED, "잘못된 응답입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 토큰을 찾을 수 없습니다."),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "해당 접근은 금지되었습니다."),
 
     // User
-    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "email not found"),
-    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "Nick name conflict"),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 이메일이 존재하지 않습니다."),
+    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
 
-    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "email conflict"),
-    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "Username Not Found"),
-    DUPLICATED_ENROLL(HttpStatus.CONFLICT, "User is Duplicate"),
-    INVALID_MAIL(HttpStatus.UNAUTHORIZED, "invalid email"),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "Role Not Found"),
+    // Brand
+    DUPLICATE_BRAND(HttpStatus.CONFLICT, "중복된 브랜드 이름입니다."),
 
+    // S3
+    WRONG_FILE_FORMAT(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일입니다"),
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
 
     ;
+
     private HttpStatus httpStatus;
     private String message;
 }
