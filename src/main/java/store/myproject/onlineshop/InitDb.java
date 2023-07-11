c3d8f7721e86e6888df2b92d8e16d6636375afb9
ohy1023 <zvyg1023@naver.com>
2023-07-11 16:48:18 +0900

    fix : ddl 생성 규칙 update시 멤버쉽이 이미 존재해 오류를 반환해 주석 처리

--- src/main/java/store/myproject/onlineshop/InitDb.java
+++ src/main/java/store/myproject/onlineshop/InitDb.java
@@ -1,64 +1,64 @@
-package store.myproject.onlineshop;
-
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import store.myproject.onlineshop.domain.customer.Level;
-import store.myproject.onlineshop.domain.membership.dto.MemberShipCreateRequest;
-import store.myproject.onlineshop.service.MemberShipService;
-
-@Component
-@RequiredArgsConstructor
-public class InitDb {
-    private final InitService initService;
-
-    @PostConstruct
-    public void init() {
-        initService.dbInit();
-    }
-
-
-    @Component
-    @Transactional
-    @RequiredArgsConstructor
-    static class InitService {
-        private final MemberShipService memberShipService;
-
-        public void dbInit() {
-            MemberShipCreateRequest bronze = MemberShipCreateRequest.builder()
-                    .baseline(0L)
-                    .level(Level.BRONZE)
-                    .discountRate(0F)
-                    .build();
-
-            memberShipService.saveMemberShip(bronze);
-
-            MemberShipCreateRequest silver = MemberShipCreateRequest.builder()
-                    .baseline(100000L)
-                    .level(Level.SILVER)
-                    .discountRate(0.1F)
-                    .build();
-
-            memberShipService.saveMemberShip(silver);
-
-            MemberShipCreateRequest gold = MemberShipCreateRequest.builder()
-                    .baseline(1000000L)
-                    .level(Level.GOLD)
-                    .discountRate(0.3F)
-                    .build();
-
-            memberShipService.saveMemberShip(gold);
-
-            MemberShipCreateRequest diamond = MemberShipCreateRequest.builder()
-                    .baseline(10000000L)
-                    .level(Level.DIAMOND)
-                    .discountRate(0.5F)
-                    .build();
-
-            memberShipService.saveMemberShip(diamond);
-        }
-    }
-
-
-}
+//package store.myproject.onlineshop;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//import store.myproject.onlineshop.domain.customer.Level;
+//import store.myproject.onlineshop.domain.membership.dto.MemberShipCreateRequest;
+//import store.myproject.onlineshop.service.MemberShipService;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class InitDb {
+//    private final InitService initService;
+//
+//    @PostConstruct
+//    public void init() {
+//        initService.dbInit();
+//    }
+//
+//
+//    @Component
+//    @Transactional
+//    @RequiredArgsConstructor
+//    static class InitService {
+//        private final MemberShipService memberShipService;
+//
+//        public void dbInit() {
+//            MemberShipCreateRequest bronze = MemberShipCreateRequest.builder()
+//                    .baseline(0L)
+//                    .level(Level.BRONZE)
+//                    .discountRate(0F)
+//                    .build();
+//
+//            memberShipService.saveMemberShip(bronze);
+//
+//            MemberShipCreateRequest silver = MemberShipCreateRequest.builder()
+//                    .baseline(100000L)
+//                    .level(Level.SILVER)
+//                    .discountRate(0.1F)
+//                    .build();
+//
+//            memberShipService.saveMemberShip(silver);
+//
+//            MemberShipCreateRequest gold = MemberShipCreateRequest.builder()
+//                    .baseline(1000000L)
+//                    .level(Level.GOLD)
+//                    .discountRate(0.3F)
+//                    .build();
+//
+//            memberShipService.saveMemberShip(gold);
+//
+//            MemberShipCreateRequest diamond = MemberShipCreateRequest.builder()
+//                    .baseline(10000000L)
+//                    .level(Level.DIAMOND)
+//                    .discountRate(0.5F)
+//                    .build();
+//
+//            memberShipService.saveMemberShip(diamond);
+//        }
+//    }
+//
+//
+//}
