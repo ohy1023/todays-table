7970aab8c1d4e21a8b19a31b819c4adbd1ce8f26
ohy1023 <zvyg1023@naver.com>
2023-07-16 19:59:11 +0900

    fix : SWAGGER에서 LocalDateTime 형식 오류 해결

--- src/main/java/store/myproject/onlineshop/domain/item/dto/ItemSearchCond.java
+++ src/main/java/store/myproject/onlineshop/domain/item/dto/ItemSearchCond.java
@@ -1,6 +1,9 @@
 package store.myproject.onlineshop.domain.item.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -19,7 +22,10 @@ public class ItemSearchCond {
 
     private Long stockLoe;
 
+    @Schema(description = "검색 시작 날짜",type = "string", example = "2023-07-16T00:00:00")
     private LocalDateTime startDate;
 
+    @Schema(description = "검색 종료 날짜",type = "string", example = "2023-07-17T00:00:00")
     private LocalDateTime endDate;
+
 }
