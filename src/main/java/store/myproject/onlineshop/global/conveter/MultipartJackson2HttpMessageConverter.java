519c067d19908a86c1832ad50ed6712d0f654270
ohy1023 <zvyg1023@naver.com>
2023-07-25 13:39:37 +0900

    style : 파일 위치 이동

--- src/main/java/store/myproject/onlineshop/global/conveter/MultipartJackson2HttpMessageConverter.java
+++ src/main/java/store/myproject/onlineshop/global/conveter/MultipartJackson2HttpMessageConverter.java
@@ -1,4 +1,4 @@
-package store.myproject.onlineshop.global.config;
+package store.myproject.onlineshop.global.conveter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
