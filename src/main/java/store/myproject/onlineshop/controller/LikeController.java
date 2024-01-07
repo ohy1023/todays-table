a2fa29b514f03dabe6830b36c9de2ea1c3576dce
ohy1023 <zvyg1023@naver.com>
2024-01-07 19:04:29 +0900

    [feat] 좋아요 개수 조회, 좋아요 누르기 기능 추가

--- src/main/java/store/myproject/onlineshop/controller/LikeController.java
+++ src/main/java/store/myproject/onlineshop/controller/LikeController.java
@@ -4,22 +4,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import store.myproject.onlineshop.domain.MessageResponse;
 import store.myproject.onlineshop.domain.Response;
 import store.myproject.onlineshop.service.RecipeService;
 
 @RequiredArgsConstructor
-@RestController("/api/v1/recipes")
+@RestController
+@RequestMapping("/api/v1/recipes")
 @Tag(name = "Recipe", description = "레시피 API")
-public class RecipeController {
+public class LikeController {
 
     private final RecipeService recipeService;
 
-    @Operation(summary = "좋아요 누르기")
+    @Operation(summary = "해당 레시피 좋아요 누르기")
     @PostMapping("/{id}/likes")
     public Response<MessageResponse> pushLike(@PathVariable Long id, Authentication authentication) {
         String email = authentication.getName();
@@ -28,7 +26,7 @@ public class RecipeController {
     }
 
 
-    @Operation(summary = "좋아요 개수 조회")
+    @Operation(summary = "해당 레세피 좋아요 개수 조회")
     @GetMapping("/{id}/likes")
     public Response<Integer> countLike(@PathVariable Long id) {
         Integer likeCnt = recipeService.countLike(id);
