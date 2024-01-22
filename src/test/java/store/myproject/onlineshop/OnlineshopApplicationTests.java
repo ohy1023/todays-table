a9e90fc2eb8ac5c7f882e5a8a43e2bda02e6d739
ohy1023 <zvyg1023@naver.com>
2024-01-22 18:39:49 +0900

    [ci] 테스트 주석 처리

--- src/test/java/store/myproject/onlineshop/OnlineshopApplicationTests.java
+++ src/test/java/store/myproject/onlineshop/OnlineshopApplicationTests.java
@@ -1,13 +1,13 @@
-package store.myproject.onlineshop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OnlineshopApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package store.myproject.onlineshop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OnlineshopApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
