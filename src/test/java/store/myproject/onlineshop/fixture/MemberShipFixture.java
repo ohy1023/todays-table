242958235d8d54a1bc2ee117aeee780fabba0a74
오형상 <vyg1023@gmail.com>
2025-04-20 12:29:13 +0900

    [Test] Add MemberShip Controller & Service Test

--- src/test/java/store/myproject/onlineshop/fixture/MemberShipFixture.java
+++ src/test/java/store/myproject/onlineshop/fixture/MemberShipFixture.java
@@ -2,6 +2,9 @@ package store.myproject.onlineshop.fixture;
 
 import store.myproject.onlineshop.domain.customer.Level;
 import store.myproject.onlineshop.domain.membership.MemberShip;
+import store.myproject.onlineshop.domain.membership.dto.MemberShipCreateRequest;
+import store.myproject.onlineshop.domain.membership.dto.MemberShipDto;
+import store.myproject.onlineshop.domain.membership.dto.MemberShipUpdateRequest;
 
 import java.math.BigDecimal;
 
@@ -42,4 +45,37 @@ public class MemberShipFixture {
                 .level(Level.DIAMOND)
                 .build();
     }
+
+    public static MemberShipDto createBronzeDto() {
+        return MemberShipDto.builder()
+                .baseline(BigDecimal.ZERO)
+                .discountRate(BigDecimal.ZERO)
+                .level(Level.BRONZE)
+                .build();
+    }
+
+    public static MemberShipDto createSilverDto() {
+        return MemberShipDto.builder()
+                .baseline(new BigDecimal("100000"))
+                .discountRate(new BigDecimal("0.1"))
+                .level(Level.SILVER)
+                .build();
+    }
+
+    public static MemberShipCreateRequest createBronzeRequest() {
+        return MemberShipCreateRequest.builder()
+                .baseline(BigDecimal.ZERO)
+                .discountRate(BigDecimal.ZERO)
+                .level(Level.BRONZE)
+                .build();
+    }
+
+    public static MemberShipUpdateRequest updateToBronzeRequest() {
+        return new MemberShipUpdateRequest(
+                Level.BRONZE,
+                new BigDecimal("50000"),
+                new BigDecimal("0.05")
+        );
+    }
+
 }
