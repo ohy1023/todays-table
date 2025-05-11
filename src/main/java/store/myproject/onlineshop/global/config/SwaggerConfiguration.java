c543bb93116e6a98a24d5fca445341c17d86ff12
오형상 <vyg1023@gmail.com>
2025-05-11 14:15:24 +0900

    [Docs] : Swagger 설명 추가

--- src/main/java/store/myproject/onlineshop/global/config/SwaggerConfiguration.java
+++ src/main/java/store/myproject/onlineshop/global/config/SwaggerConfiguration.java
@@ -25,17 +25,25 @@ public class SwaggerConfiguration {
                 .info(new Info()
                         .title("오늘의 식탁 ")
                         .description("오늘의 식탁은 레시피를 기반으로 사용자가 식재료를 쉽게 구매할 수 있도록 지원하는 백엔드 API 서비스입니다.\n\n"
-                                + "### ADMIN 테스트 계정 정보\n"
-                                + "Email: zvyg1023@naver.com\n"
-                                + "Password: 1Q2w3e4r!!\n"
-                                + "권한: ROLE_ADMIN\n\n"
-                                + "### 주문 순서\n"
-                                + "1. 단건 주문.\n"
-                                + "2. 사전 검증.\n"
-                                + "3. 사후 검증.\n"
-                                + "### 주의 사항\n"
-                                + "IMP_UID는 포트원에서 발급받은 고유 ID로, 프론트엔드에서 받아서 백엔드로 전달해야 합니다.\n"
-                                + "이에 따라 IMP_UID가 사용되는 API(사후 검증, 주문 취소)는 테스트가 불가합니다.\n\n")
+                                + "### 🔐 ADMIN 테스트 계정 정보\n"
+                                + "- Email: zvyg1023@naver.com\n"
+                                + "- Password: 1Q2w3e4r!!\n"
+                                + "- 권한: ROLE_ADMIN\n\n"
+                                + "### 🛒 주문 흐름\n"
+                                + "1. 단건 주문\n"
+                                + "2. 사전 검증\n"
+                                + "3. 사후 검증\n\n"
+                                + "### 🔑 인증 방식 (JWT)\n"
+                                + "- 로그인 후 응답받은 토큰(Access Token)을 사용합니다.\n"
+                                + "- Swagger UI 우측 상단의 Authorize 버튼을 눌러,\n"
+                                + "  `Bearer` 접두어 없이 순수 토큰 값만 입력하세요.\n"
+                                + "  예시: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...`\n\n"
+                                + "### ⚠️ 테스트 시 유의 사항\n"
+                                + "- IMP_UID는 포트원(PortOne)에서 프론트엔드가 받아 백엔드로 전달해야 합니다.\n"
+                                + "- 따라서 IMP_UID가 필요한 API(사후 검증, 주문 취소)는 Swagger에서 직접 테스트할 수 없습니다.\n\n"
+                                + "### 🚨 속도 관련 안내\n"
+                                + "- 현재 본 프로젝트는 **Cloudtype 무료 배포 환경**에서 서비스되고 있어,\n"
+                                + "  Swagger UI의 초기 로딩 및 응답 속도가 느릴 수 있습니다.\n")
                         .version(" 1.0.0 ")
                 )
                 .addSecurityItem(new SecurityRequirement().addList(defaultHeader))
