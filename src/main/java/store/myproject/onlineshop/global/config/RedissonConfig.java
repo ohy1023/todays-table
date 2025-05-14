09ebac3a47f746f8a001310ded30d416e6a91b11
오형상 <vyg1023@gmail.com>
2025-05-14 19:57:33 +0900

    [Feat] : Cache Redis 설정

---
+++ src/main/java/store/myproject/onlineshop/global/config/RedissonConfig.java
@@ -0,0 +1,32 @@
+package store.myproject.onlineshop.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.data.redis.cache.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.cache.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.cache.password:}")
+    private String redisPassword;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
+                .setPassword(redisPassword);
+        return Redisson.create(config);
+    }
+}
