39982e72d4a924b7e6975ed2e0cca1d52dd8c395
오형상 <vyg1023@gmail.com>
2025-08-02 11:10:56 +0900

    [Feat] Slave 2대 -> 1대 수정

--- src/main/java/store/myproject/onlineshop/global/db/DataSourceConfiguration.java
+++ src/main/java/store/myproject/onlineshop/global/db/DataSourceConfiguration.java
@@ -18,8 +18,7 @@ import java.util.Map;
 public class DataSourceConfiguration {
 
     public static final String MASTER_DATASOURCE = "masterDataSource";
-    public static final String SLAVE1_DATASOURCE = "slave1DataSource";
-    public static final String SLAVE2_DATASOURCE = "slave2DataSource";
+    public static final String SLAVE_DATASOURCE = "slaveDataSource";
 
     @Bean(MASTER_DATASOURCE)
     @ConfigurationProperties("spring.datasource.master.hikari")
@@ -27,28 +26,22 @@ public class DataSourceConfiguration {
         return new HikariDataSource();
     }
 
-    @Bean(SLAVE1_DATASOURCE)
-    @ConfigurationProperties("spring.datasource.slave1.hikari")
+    @Bean(SLAVE_DATASOURCE)
+    @ConfigurationProperties("spring.datasource.slave.hikari")
     public HikariDataSource slaveDataSource() {
         return new HikariDataSource();
     }
 
-    @Bean(SLAVE2_DATASOURCE)
-    @ConfigurationProperties("spring.datasource.slave2.hikari")
-    public HikariDataSource slave2DataSource() {return new HikariDataSource();}
-
     @Bean
     public DataSource routingDataSource(
             @Qualifier(MASTER_DATASOURCE) DataSource masterDataSource,
-            @Qualifier(SLAVE1_DATASOURCE) DataSource slave1DataSource,
-            @Qualifier(SLAVE2_DATASOURCE) DataSource slave2DataSource) {
+            @Qualifier(SLAVE_DATASOURCE) DataSource slave1DataSource) {
 
         RoutingDataSource routingDataSource = new RoutingDataSource();
 
         Map<Object, Object> dataSourceMap = new HashMap<>();
         dataSourceMap.put("master", masterDataSource);
-        dataSourceMap.put("slave1", slave1DataSource);
-        dataSourceMap.put("slave2", slave2DataSource);
+        dataSourceMap.put("slave", slave1DataSource);
 
         routingDataSource.setTargetDataSources(Collections.unmodifiableMap(dataSourceMap));
         routingDataSource.setDefaultTargetDataSource(masterDataSource);
--- src/main/java/store/myproject/onlineshop/global/db/RoutingDataSource.java
+++ src/main/java/store/myproject/onlineshop/global/db/RoutingDataSource.java
@@ -4,31 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 @Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
-    private static final String MASTER = "master";
-    private static final List<String> SLAVES = List.of("slave1", "slave2");
-
-    private final AtomicInteger slaveIndex = new AtomicInteger(0);
-
     @Override
+    // 현재 데이터베이스 연결을 결정하기 위해 호출하는 메서드
     protected Object determineCurrentLookupKey() {
+
         boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
 
+        log.info("읽기 유뮤 : {}", isReadOnly);
+
         if (isReadOnly) {
-
-            int index = Math.abs(slaveIndex.getAndIncrement() % SLAVES.size());
-            String selectedSlave = SLAVES.get(index);
-
-            log.info("읽기 트랜잭션 -> {} DataSource 선택", selectedSlave);
-            return selectedSlave;
+            log.info("Slave DataSource 호출 ");
         } else {
-            log.info("쓰기 트랜잭션 -> master DataSource 선택");
-            return MASTER;
+            log.info("Master DataSource 호출 ");
         }
+
+        // 현재 트랜잭션이 읽기 전용인 경우는 slave, 아닐 경우 master를 반환한다 -> 트랜잭션의 속성에 따라 데이터베이스 연결을 결정
+        return isReadOnly ? "slave" : "master";
     }
 }
