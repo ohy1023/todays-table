b9bf81f1cc93485fdb8ff0f86e2328e7e90d7dcd
오형상 <vyg1023@gmail.com>
2025-08-02 11:10:24 +0900

    [Feat] Recipe Index 추가

--- src/main/resources/index.sql
+++ src/main/resources/index.sql
@@ -42,15 +42,54 @@ DEALLOCATE PREPARE stmt_item;
 #     FROM INFORMATION_SCHEMA.STATISTICS
 #     WHERE table_schema = DATABASE()
 #       AND table_name = 'Orders'
-#       AND index_name = 'idx_orders_status_createddate'
+#       AND index_name = 'idx_orders_status_created_date'
 # );
 #
 # SET @create_index_orders := IF(
 #         @index_exists = 0,
-#         'ALTER TABLE Orders ADD INDEX idx_orders_status_createddate (order_status, created_date);',
+#         'ALTER TABLE Orders ADD INDEX idx_orders_status_created_date (order_status, created_date);',
 #         'SELECT "Index already exists for Orders"'
 #                             );
 #
 # PREPARE stmt_orders FROM @create_index_orders;
 # EXECUTE stmt_orders;
 # DEALLOCATE PREPARE stmt_orders;
+
+
+-- Recipe 인덱스 조건부 생성: (recipe_servings, recipe_cooking_time)
+SET @index_exists := (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE table_schema = DATABASE()
+      AND table_name = 'Recipe'
+      AND index_name = 'idx_recipe_servings_cooking_time'
+);
+
+SET @create_index_recipe := IF(
+        @index_exists = 0,
+        'ALTER TABLE Recipe ADD INDEX idx_recipe_servings_cooking_time (recipe_servings, recipe_cooking_time);',
+        'SELECT "Index already exists for Recipe (servings, cooking_time)"'
+                            );
+
+PREPARE stmt_recipe FROM @create_index_recipe;
+EXECUTE stmt_recipe;
+DEALLOCATE PREPARE stmt_recipe;
+
+-- Recipe 인덱스 조건부 생성: (created_date)
+SET @index_exists := (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE table_schema = DATABASE()
+      AND table_name = 'Recipe'
+      AND index_name = 'idx_recipe_created_date'
+);
+
+SET @create_index_recipe_created := IF(
+        @index_exists = 0,
+        'ALTER TABLE Recipe ADD INDEX idx_recipe_created_date (created_date);',
+        'SELECT "Index already exists for Recipe (created_date)"'
+                                    );
+
+PREPARE stmt_recipe_created FROM @create_index_recipe_created;
+EXECUTE stmt_recipe_created;
+DEALLOCATE PREPARE stmt_recipe_created;
